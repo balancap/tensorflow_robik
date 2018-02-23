cbb738d09165c4a8ce5613918a283b1591de0f11
Paul Balanca <paul.balanca@gmail.com>
2018-02-23 12:38:58 +0000

    UPDATE: reorganize the kernels...

--- tensorflow/core/kernels/hex_nn/grad_depthwise_conv_op_gpu.cu.cc
+++ tensorflow/core/kernels/hex_nn/grad_depthwise_conv_op_gpu.cu.cc
@@ -38,35 +38,14 @@ namespace tensorflow {
 
 using Eigen::GpuDevice;
 
-// Returns whether depthwise convolution forward or backward input pass can be
-// performed using the faster ('Small') variant of the kernel.
-EIGEN_DEVICE_FUNC bool CanLaunchGradDepthwiseConv2dGPUSmall(
-    const GradDepthwiseArgs args) {
-  return args.depth_multiplier == 1 && args.stride == 1 && args.in_rows <= 32 &&
-         args.in_cols <= 32 && args.in_rows == args.out_rows &&
-         args.in_cols == args.out_cols && args.pad_rows >= 0 &&
-         args.pad_rows < args.filter_rows && args.pad_cols >= 0 &&
-         args.pad_cols < args.filter_cols &&
-         args.filter_rows * args.filter_cols <=
-             (args.in_rows + 1) / 2 * args.in_cols;
-}
-
-// Returns whether depthwise convolution backward filter pass can be performed
-// using the faster ('Small') variant of the kernel.
-EIGEN_DEVICE_FUNC bool CanLaunchGradDepthwiseConv2dBackpropFilterGPUSmall(
-    const GradDepthwiseArgs args, const int block_rows) {
-  return args.depth_multiplier == 1 && args.stride == 1 && args.in_rows <= 32 &&
-         args.in_cols <= 32 && args.in_rows == args.out_rows &&
-         args.in_cols == args.out_cols && args.pad_rows >= 0 &&
-         args.pad_rows < args.filter_rows && args.pad_cols >= 0 &&
-         args.pad_cols < args.filter_cols && block_rows <= args.in_rows &&
-         args.filter_rows * args.filter_cols <= args.in_cols * block_rows;
-}
-
 // The GradDepthwiseConv2dGPUKernels perform either forward or backprop input
 // convolution depending on a template argument of this enum.
 enum GradDepthwiseConv2dDirection { DIRECTION_FORWARD, DIRECTION_BACKWARD };
 
+
+// -------------------------------------------------------------------------- //
+// Implementation of depthwise conv2d.
+// -------------------------------------------------------------------------- //
 // A Cuda kernel to compute the depthwise convolution forward pass
 // in NHWC format.
 template <typename T, int kKnownFilterWidth, int kKnownFilterHeight,
@@ -147,6 +126,195 @@ __global__ void __launch_bounds__(1024, 2)
     }
     output[thread_id] = sum;
   }
+}
+
+// A Cuda kernel to compute the depthwise convolution backprop w.r.t. input.
+template <typename T, int kKnownFilterWidth, int kKnownFilterHeight,
+          int kKnownDepthMultiplier>
+__global__ void __launch_bounds__(640, 2)
+    GradDepthwiseConv2dBackpropInputGPUKernelNHWC(const GradDepthwiseArgs args,
+                                              const T* out_backprop,
+                                              const T* filter, T* in_backprop,
+                                              int num_in_backprop) {
+  const int in_rows = args.in_rows;
+  const int in_cols = args.in_cols;
+  const int in_depth = args.in_depth;
+  const int filter_rows =
+      kKnownFilterHeight < 0 ? args.filter_rows : kKnownFilterHeight;
+  const int filter_cols =
+      kKnownFilterWidth < 0 ? args.filter_cols : kKnownFilterWidth;
+  const int depth_multiplier =
+      kKnownDepthMultiplier < 0 ? args.depth_multiplier : kKnownDepthMultiplier;
+  const int stride = args.stride;
+  const int pad_rows = args.pad_rows;
+  const int pad_cols = args.pad_cols;
+  const int out_rows = args.out_rows;
+  const int out_cols = args.out_cols;
+  const int out_depth = args.out_depth;
+
+  CUDA_1D_KERNEL_LOOP(thread_id, num_in_backprop) {
+    // Compute the indexes of this thread in the output.
+    const int in_d = thread_id % in_depth;
+    const int in_c = (thread_id / in_depth) % in_cols;
+    const int in_r = (thread_id / in_depth / in_cols) % in_rows;
+    const int b = thread_id / in_depth / in_cols / in_rows;
+
+    T sum = static_cast<T>(0);
+
+    const int out_r_start =
+        tf_max<int>(0, (in_r - filter_rows + pad_rows + stride) / stride);
+    const int out_r_end = tf_min(out_rows - 1, (in_r + pad_rows) / stride);
+    const int out_c_start =
+        tf_max(0, (in_c - filter_cols + pad_cols + stride) / stride);
+    const int out_c_end = tf_min(out_cols - 1, (in_c + pad_cols) / stride);
+
+    NOUNROLL for (int out_r = out_r_start; out_r <= out_r_end; ++out_r) {
+      const int f_r = in_r + pad_rows - out_r * stride;
+      const int temp_out_backprop_offset =
+          out_depth * out_cols * (out_r + out_rows * b);
+      const int temp_filter_offset = filter_cols * f_r;
+      NOUNROLL for (int out_c = out_c_start; out_c <= out_c_end; ++out_c) {
+        const int f_c = in_c + pad_cols - out_c * stride;
+        int filter_offset =
+            depth_multiplier * (in_d + in_depth * (f_c + temp_filter_offset));
+        const int out_backprop_offset =
+            out_depth * out_c + temp_out_backprop_offset;
+#pragma unroll 6
+        for (int i = 0; i < depth_multiplier; ++i) {
+          sum += ldg(out_backprop + out_backprop_offset +
+                     in_d * depth_multiplier + i) *
+                 ldg(filter + filter_offset + i);
+        }
+      }
+    }
+    const int in_backprop_offset =
+        in_d + in_depth * (in_c + in_cols * (in_r + in_rows * b));
+    in_backprop[in_backprop_offset] = sum;
+  }
+}
+
+// A Cuda kernel to compute the depthwise convolution backprop w.r.t. filter.
+template <typename T, int kKnownFilterWidth, int kKnownFilterHeight,
+          int kKnownDepthMultiplier>
+__global__ void __launch_bounds__(640, 2)
+    GradDepthwiseConv2dBackpropFilterGPUKernelNHWC(const GradDepthwiseArgs args,
+                                               const T* out_backprop,
+                                               const T* input,
+                                               T* filter_backprop,
+                                               int num_out_backprop) {
+  const int in_rows = args.in_rows;
+  const int in_cols = args.in_cols;
+  const int in_depth = args.in_depth;
+  const int filter_rows =
+      kKnownFilterHeight < 0 ? args.filter_rows : kKnownFilterHeight;
+  const int filter_cols =
+      kKnownFilterWidth < 0 ? args.filter_cols : kKnownFilterWidth;
+  const int depth_multiplier =
+      kKnownDepthMultiplier < 0 ? args.depth_multiplier : kKnownDepthMultiplier;
+  const int stride = args.stride;
+  const int pad_rows = args.pad_rows;
+  const int pad_cols = args.pad_cols;
+  const int out_rows = args.out_rows;
+  const int out_cols = args.out_cols;
+  const int out_depth = args.out_depth;
+
+  CUDA_1D_KERNEL_LOOP(thread_id, num_out_backprop) {
+    // Compute the indexes of this thread in the output.
+    const int out_d = thread_id % out_depth;
+    const int out_c = (thread_id / out_depth) % out_cols;
+    const int out_r = (thread_id / out_depth / out_cols) % out_rows;
+    const int b = thread_id / out_depth / out_cols / out_rows;
+    // Compute the input depth and the index of depth multiplier.
+    const int in_d = out_d / depth_multiplier;
+    const int dm = out_d % depth_multiplier;
+
+    // Decide if all input is valid, if yes, we can skip the boundary checks
+    // for each input.
+    const int in_r_start = out_r * stride - pad_rows;
+    const int in_c_start = out_c * stride - pad_cols;
+    const int in_r_end = in_r_start + filter_rows;
+    const int in_c_end = in_c_start + filter_cols;
+
+    const int out_backprop_offset =
+        out_d + out_depth * (out_c + out_cols * (out_r + out_rows * b));
+    const T out_bp = ldg(out_backprop + out_backprop_offset);
+    if (in_r_start >= 0 && in_c_start >= 0 && in_r_end < in_rows &&
+        in_c_end < in_cols) {
+      UNROLL for (int f_r = 0; f_r < filter_rows; ++f_r) {
+        const int in_r = in_r_start + f_r;
+        // Avoid repeated computation.
+        const int input_offset_temp = in_cols * (in_r + in_rows * b);
+        UNROLL for (int f_c = 0; f_c < filter_cols; ++f_c) {
+          const int in_c = in_c_start + f_c;
+
+          const int input_offset = in_d + in_depth * (in_c + input_offset_temp);
+          T partial_sum = ldg(input + input_offset) * out_bp;
+          T* addr = filter_backprop +
+                    (dm + depth_multiplier *
+                              (in_d + in_depth * (f_c + filter_cols * f_r)));
+          CudaAtomicAdd(addr, partial_sum);
+        }
+      }
+    } else {
+      UNROLL for (int f_r = 0; f_r < filter_rows; ++f_r) {
+        const int in_r = in_r_start + f_r;
+        // Avoid repeated computation.
+        const int input_offset_temp = in_cols * (in_r + in_rows * b);
+        UNROLL for (int f_c = 0; f_c < filter_cols; ++f_c) {
+          const int in_c = in_c_start + f_c;
+          const int addr_temp = filter_cols * f_r;
+
+          if (in_r >= 0 && in_r < in_rows && in_c >= 0 && in_c < in_cols) {
+            const int input_offset =
+                in_d + in_depth * (in_c + input_offset_temp);
+            T partial_sum = ldg(input + input_offset) * out_bp;
+            T* addr =
+                filter_backprop +
+                (dm + depth_multiplier * (in_d + in_depth * (f_c + addr_temp)));
+            // Potentially many threads can add to the same address so we have
+            // to use atomic add here.
+            // TODO(jmchen): If atomic add turns out to be slow, we can:
+            // 1. allocate multiple buffers for the gradients (one for each
+            // example in a batch, for example). This can reduce the
+            // contention on the destination; 2. Have each thread compute one
+            // gradient for an element in the filters. This should work well
+            // when the input depth is big and filter size is not too small.
+            CudaAtomicAdd(addr, partial_sum);
+          }
+        }
+      }
+    }
+  }
+}
+
+
+// -------------------------------------------------------------------------- //
+// Old stuff...
+// -------------------------------------------------------------------------- //
+
+// Returns whether depthwise convolution forward or backward input pass can be
+// performed using the faster ('Small') variant of the kernel.
+EIGEN_DEVICE_FUNC bool CanLaunchGradDepthwiseConv2dGPUSmall(
+    const GradDepthwiseArgs args) {
+  return false && args.depth_multiplier == 1 && args.stride == 1 && args.in_rows <= 32 &&
+         args.in_cols <= 32 && args.in_rows == args.out_rows &&
+         args.in_cols == args.out_cols && args.pad_rows >= 0 &&
+         args.pad_rows < args.filter_rows && args.pad_cols >= 0 &&
+         args.pad_cols < args.filter_cols &&
+         args.filter_rows * args.filter_cols <=
+             (args.in_rows + 1) / 2 * args.in_cols;
+}
+
+// Returns whether depthwise convolution backward filter pass can be performed
+// using the faster ('Small') variant of the kernel.
+EIGEN_DEVICE_FUNC bool CanLaunchGradDepthwiseConv2dBackpropFilterGPUSmall(
+    const GradDepthwiseArgs args, const int block_rows) {
+  return false && args.depth_multiplier == 1 && args.stride == 1 && args.in_rows <= 32 &&
+         args.in_cols <= 32 && args.in_rows == args.out_rows &&
+         args.in_cols == args.out_cols && args.pad_rows >= 0 &&
+         args.pad_rows < args.filter_rows && args.pad_cols >= 0 &&
+         args.pad_cols < args.filter_cols && block_rows <= args.in_rows &&
+         args.filter_rows * args.filter_cols <= args.in_cols * block_rows;
 }
 
 // CUDA kernel to compute the depthwise convolution forward pass in NHWC format,
@@ -716,70 +884,7 @@ template struct LaunchGradDepthwiseConvOp<GPUDevice, Eigen::half>;
 template struct LaunchGradDepthwiseConvOp<GPUDevice, float>;
 template struct LaunchGradDepthwiseConvOp<GPUDevice, double>;
 
-// A Cuda kernel to compute the depthwise convolution backprop w.r.t. input.
-template <typename T, int kKnownFilterWidth, int kKnownFilterHeight,
-          int kKnownDepthMultiplier>
-__global__ void __launch_bounds__(640, 2)
-    GradDepthwiseConv2dBackpropInputGPUKernelNHWC(const GradDepthwiseArgs args,
-                                              const T* out_backprop,
-                                              const T* filter, T* in_backprop,
-                                              int num_in_backprop) {
-  const int in_rows = args.in_rows;
-  const int in_cols = args.in_cols;
-  const int in_depth = args.in_depth;
-  const int filter_rows =
-      kKnownFilterHeight < 0 ? args.filter_rows : kKnownFilterHeight;
-  const int filter_cols =
-      kKnownFilterWidth < 0 ? args.filter_cols : kKnownFilterWidth;
-  const int depth_multiplier =
-      kKnownDepthMultiplier < 0 ? args.depth_multiplier : kKnownDepthMultiplier;
-  const int stride = args.stride;
-  const int pad_rows = args.pad_rows;
-  const int pad_cols = args.pad_cols;
-  const int out_rows = args.out_rows;
-  const int out_cols = args.out_cols;
-  const int out_depth = args.out_depth;
 
-  CUDA_1D_KERNEL_LOOP(thread_id, num_in_backprop) {
-    // Compute the indexes of this thread in the output.
-    const int in_d = thread_id % in_depth;
-    const int in_c = (thread_id / in_depth) % in_cols;
-    const int in_r = (thread_id / in_depth / in_cols) % in_rows;
-    const int b = thread_id / in_depth / in_cols / in_rows;
-
-    T sum = static_cast<T>(0);
-
-    const int out_r_start =
-        tf_max<int>(0, (in_r - filter_rows + pad_rows + stride) / stride);
-    const int out_r_end = tf_min(out_rows - 1, (in_r + pad_rows) / stride);
-    const int out_c_start =
-        tf_max(0, (in_c - filter_cols + pad_cols + stride) / stride);
-    const int out_c_end = tf_min(out_cols - 1, (in_c + pad_cols) / stride);
-
-    NOUNROLL for (int out_r = out_r_start; out_r <= out_r_end; ++out_r) {
-      const int f_r = in_r + pad_rows - out_r * stride;
-      const int temp_out_backprop_offset =
-          out_depth * out_cols * (out_r + out_rows * b);
-      const int temp_filter_offset = filter_cols * f_r;
-      NOUNROLL for (int out_c = out_c_start; out_c <= out_c_end; ++out_c) {
-        const int f_c = in_c + pad_cols - out_c * stride;
-        int filter_offset =
-            depth_multiplier * (in_d + in_depth * (f_c + temp_filter_offset));
-        const int out_backprop_offset =
-            out_depth * out_c + temp_out_backprop_offset;
-#pragma unroll 6
-        for (int i = 0; i < depth_multiplier; ++i) {
-          sum += ldg(out_backprop + out_backprop_offset +
-                     in_d * depth_multiplier + i) *
-                 ldg(filter + filter_offset + i);
-        }
-      }
-    }
-    const int in_backprop_offset =
-        in_d + in_depth * (in_c + in_cols * (in_r + in_rows * b));
-    in_backprop[in_backprop_offset] = sum;
-  }
-}
 
 template <typename T, int kKnownFilterWidth, int kKnownFilterHeight,
           int kKnownDepthMultiplier>
@@ -925,100 +1030,6 @@ void LaunchGradDepthwiseConvBackpropInputOp<GPUDevice, T>::operator()(
 template struct LaunchGradDepthwiseConvBackpropInputOp<GPUDevice, Eigen::half>;
 template struct LaunchGradDepthwiseConvBackpropInputOp<GPUDevice, float>;
 template struct LaunchGradDepthwiseConvBackpropInputOp<GPUDevice, double>;
-
-// A Cuda kernel to compute the depthwise convolution backprop w.r.t. filter.
-template <typename T, int kKnownFilterWidth, int kKnownFilterHeight,
-          int kKnownDepthMultiplier>
-__global__ void __launch_bounds__(640, 2)
-    GradDepthwiseConv2dBackpropFilterGPUKernelNHWC(const GradDepthwiseArgs args,
-                                               const T* out_backprop,
-                                               const T* input,
-                                               T* filter_backprop,
-                                               int num_out_backprop) {
-  const int in_rows = args.in_rows;
-  const int in_cols = args.in_cols;
-  const int in_depth = args.in_depth;
-  const int filter_rows =
-      kKnownFilterHeight < 0 ? args.filter_rows : kKnownFilterHeight;
-  const int filter_cols =
-      kKnownFilterWidth < 0 ? args.filter_cols : kKnownFilterWidth;
-  const int depth_multiplier =
-      kKnownDepthMultiplier < 0 ? args.depth_multiplier : kKnownDepthMultiplier;
-  const int stride = args.stride;
-  const int pad_rows = args.pad_rows;
-  const int pad_cols = args.pad_cols;
-  const int out_rows = args.out_rows;
-  const int out_cols = args.out_cols;
-  const int out_depth = args.out_depth;
-
-  CUDA_1D_KERNEL_LOOP(thread_id, num_out_backprop) {
-    // Compute the indexes of this thread in the output.
-    const int out_d = thread_id % out_depth;
-    const int out_c = (thread_id / out_depth) % out_cols;
-    const int out_r = (thread_id / out_depth / out_cols) % out_rows;
-    const int b = thread_id / out_depth / out_cols / out_rows;
-    // Compute the input depth and the index of depth multiplier.
-    const int in_d = out_d / depth_multiplier;
-    const int dm = out_d % depth_multiplier;
-
-    // Decide if all input is valid, if yes, we can skip the boundary checks
-    // for each input.
-    const int in_r_start = out_r * stride - pad_rows;
-    const int in_c_start = out_c * stride - pad_cols;
-    const int in_r_end = in_r_start + filter_rows;
-    const int in_c_end = in_c_start + filter_cols;
-
-    const int out_backprop_offset =
-        out_d + out_depth * (out_c + out_cols * (out_r + out_rows * b));
-    const T out_bp = ldg(out_backprop + out_backprop_offset);
-    if (in_r_start >= 0 && in_c_start >= 0 && in_r_end < in_rows &&
-        in_c_end < in_cols) {
-      UNROLL for (int f_r = 0; f_r < filter_rows; ++f_r) {
-        const int in_r = in_r_start + f_r;
-        // Avoid repeated computation.
-        const int input_offset_temp = in_cols * (in_r + in_rows * b);
-        UNROLL for (int f_c = 0; f_c < filter_cols; ++f_c) {
-          const int in_c = in_c_start + f_c;
-
-          const int input_offset = in_d + in_depth * (in_c + input_offset_temp);
-          T partial_sum = ldg(input + input_offset) * out_bp;
-          T* addr = filter_backprop +
-                    (dm + depth_multiplier *
-                              (in_d + in_depth * (f_c + filter_cols * f_r)));
-          CudaAtomicAdd(addr, partial_sum);
-        }
-      }
-    } else {
-      UNROLL for (int f_r = 0; f_r < filter_rows; ++f_r) {
-        const int in_r = in_r_start + f_r;
-        // Avoid repeated computation.
-        const int input_offset_temp = in_cols * (in_r + in_rows * b);
-        UNROLL for (int f_c = 0; f_c < filter_cols; ++f_c) {
-          const int in_c = in_c_start + f_c;
-          const int addr_temp = filter_cols * f_r;
-
-          if (in_r >= 0 && in_r < in_rows && in_c >= 0 && in_c < in_cols) {
-            const int input_offset =
-                in_d + in_depth * (in_c + input_offset_temp);
-            T partial_sum = ldg(input + input_offset) * out_bp;
-            T* addr =
-                filter_backprop +
-                (dm + depth_multiplier * (in_d + in_depth * (f_c + addr_temp)));
-            // Potentially many threads can add to the same address so we have
-            // to use atomic add here.
-            // TODO(jmchen): If atomic add turns out to be slow, we can:
-            // 1. allocate multiple buffers for the gradients (one for each
-            // example in a batch, for example). This can reduce the
-            // contention on the destination; 2. Have each thread compute one
-            // gradient for an element in the filters. This should work well
-            // when the input depth is big and filter size is not too small.
-            CudaAtomicAdd(addr, partial_sum);
-          }
-        }
-      }
-    }
-  }
-}
 
 // Device function to compute sub-warp sum reduction for a power-of-two group of
 // neighboring threads.
