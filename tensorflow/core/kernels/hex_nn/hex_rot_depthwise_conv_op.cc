cdb65163fbaa78725936af32c5358bac82400cdd
Paul B <paul.balanca@gmail.com>
2018-03-21 16:29:16 +0000

    CLEAN: remove CPUDevice implementations...

--- tensorflow/core/kernels/hex_nn/hex_rot_depthwise_conv_op.cc
+++ tensorflow/core/kernels/hex_nn/hex_rot_depthwise_conv_op.cc
@@ -56,193 +56,6 @@ namespace tensorflow {
 typedef Eigen::ThreadPoolDevice CPUDevice;
 typedef Eigen::GpuDevice GPUDevice;
 
-// Computes the vectorized product of 'input_buffer' and 'filter' and stores
-// result in 'output' at location specified by 'out_r' and 'out_c'.
-//
-// EX:
-//   in_depth = 3, depth_multiplier = 2, filter [2, 2], register_width = 4
-//   Both 'input_buffer' and 'filter' are padded to register-width boundaries.
-//
-//   input_buffer [rows, cols, in_depth, depth_multiplier]
-//     [a0, a0, a1, a1] [a2, a2, 0, 0] [b0, b0, b1, b1] [b2, b2, 0, 0]
-//     [e0, e0, e1, e1] [e2, e2, 0, 0] [f0, f0, f1, f1] [f2, f2, 0, 0]
-//
-//   filter [rows, cols, in_depth, depth_multiplier]
-//     [u0, v0, w0, x0] [y0, z0, 0, 0] [u1, v1, w1, x1] [y1, z1, 0, 0]
-//     [u2, v2, w2, x2] [y2, z2, 0, 0] [u3, v3, w3, x3] [y3, z3, 0, 0]
-//
-//   First output register [in_depth, depth_multiplier]
-//     [q0, q1, q2, q3] = ([a0, a0, a1, a1] x [u0, v0, w0, x0]) +
-//                        ([b0, b0, b1, b1] x [u1, v1, w1, x1]) +
-//                        ([e0, e0, e1, e1] x [u2, v2, w2, x2]) +
-//                        ([f0, f0, f1, f1] x [u3, v3, w3, x3])
-//
-// TODO(andydavis) Experiment with processing multiple inputs per input buffer.
-template <typename T>
-struct HexRotDepthwiseConv2DKernel {
-  static void Run(const HexRotDepthwiseArgs& args,
-                  const int64 padded_filter_inner_dim_size, const int64 out_r,
-                  const int64 out_c, const T* filter, const T* input_buffer,
-                  T* output, TensorFormat data_format) {
-    typedef typename Eigen::internal::packet_traits<T>::type Packet;
-    static const int64 kPacketSize = (sizeof(Packet) / sizeof(T));
-
-    const int64 out_depth = args.out_depth;
-    const int64 filter_spatial_size = args.filter_rows * args.filter_cols;
-    const int64 output_scalar_size = out_depth % kPacketSize;
-    const int64 output_vectorized_size =
-        (out_depth / kPacketSize) * kPacketSize;
-    const int64 base_output_index = (out_r * args.out_cols + out_c) * out_depth;
-
-    for (int i = 0; i < output_vectorized_size; i += kPacketSize) {
-      // Reset accumulator.
-      auto vaccum = Eigen::internal::pset1<Packet>(static_cast<T>(0));
-      for (int j = 0; j < filter_spatial_size; ++j) {
-        // Calculate index.
-        const int64 index = i + j * padded_filter_inner_dim_size;
-        // Load filter.
-        // TODO(andydavis) Unroll 'out_c' loop in caller so we can load
-        // multiple inputs here to amortize the cost of each filter block load.
-        const auto filter_block =
-            Eigen::internal::ploadu<Packet>(filter + index);
-        // Load input.
-        const auto data_block =
-            Eigen::internal::ploadu<Packet>(input_buffer + index);
-        // Vector multiply-add.
-        vaccum =
-            Eigen::internal::pmadd<Packet>(filter_block, data_block, vaccum);
-      }
-      // Store vector accumulator to output.
-      Eigen::internal::pstoreu<T>(output + base_output_index + i, vaccum);
-    }
-
-    if (output_scalar_size > 0) {
-      auto vaccum = Eigen::internal::pset1<Packet>(static_cast<T>(0));
-      for (int j = 0; j < filter_spatial_size; ++j) {
-        const int64 index =
-            output_vectorized_size + j * padded_filter_inner_dim_size;
-        const auto filter_block =
-            Eigen::internal::ploadu<Packet>(filter + index);
-        const auto data_block =
-            Eigen::internal::ploadu<Packet>(input_buffer + index);
-        vaccum =
-            Eigen::internal::pmadd<Packet>(filter_block, data_block, vaccum);
-      }
-      // Load accumulator into an array and loop through output.
-      T out_buf[kPacketSize];
-      Eigen::internal::pstoreu<T>(out_buf, vaccum);
-      const int64 last_output_index =
-          base_output_index + output_vectorized_size;
-      for (int j = 0; j < output_scalar_size; ++j) {
-        output[last_output_index + j] = out_buf[j];
-        output[last_output_index + j] = static_cast<T>(0);
-      }
-    }
-  }
-};
-
-// Computes the depthwise conv2d of 'input' by 'depthwise_filter' and stores
-// the result in 'output'. This implementation trades off copying small patches
-// of the input to achieve better data alignment, which enables vectorized
-// load/store and multiply-add operations (see comments at InputBufferCopyOp and
-// HexRotDepthwiseConv2DKernel for details).
-//
-// TODO(andydavis) Evaluate the performance of processing multiple input
-// patches in the inner loop.
-// TODO(andydavis) Consider a zero-copy implementation for the case when
-// 'in_depth' is a multiple of register width, and 'depth_multipler' is one.
-// TODO(andydavis) Evaluate the performance of alternative implementations.
-template <typename T>
-struct LaunchHexRotDepthwiseConvOp<CPUDevice, T> {
-  typedef typename Eigen::internal::packet_traits<T>::type Packet;
-
-  void operator()(OpKernelContext* ctx, const HexRotDepthwiseArgs& args,
-                  const T* input, const T* depthwise_filter, T* output,
-                  TensorFormat data_format) {
-    OP_REQUIRES(
-        ctx, data_format == FORMAT_NHWC,
-        errors::Unimplemented(
-            "Depthwise convolution on CPU is only supported for NHWC format"));
-    static const int64 kPacketSize = (sizeof(Packet) / sizeof(T));
-
-    // Pad 'depthwise_filter' to vector register width (if needed).
-    const bool pad_filter = (args.out_depth % kPacketSize) == 0 ? false : true;
-    Tensor padded_filter;
-    if (pad_filter) {
-      // Allocate space for padded filter.
-      const int64 filter_spatial_size = args.filter_rows * args.filter_cols;
-      const int64 padded_filter_inner_dim_size =
-          ((args.out_depth + kPacketSize - 1) / kPacketSize) * kPacketSize;
-      OP_REQUIRES_OK(
-          ctx, ctx->allocate_temp(DataTypeToEnum<T>::value,
-                                  TensorShape({filter_spatial_size,
-                                               padded_filter_inner_dim_size}),
-                                  &padded_filter));
-      // Write out padded filter.
-      functor::HexRotDepthwiseFilterPadOp<T>()(
-          args, depthwise_filter, padded_filter.template flat<T>().data());
-    }
-    const T* filter_data =
-        pad_filter ? padded_filter.template flat<T>().data() : depthwise_filter;
-
-    // Computes one shard of depthwise conv2d output.
-    auto shard = [&ctx, &args, &input, &filter_data, &output, data_format](
-                     int64 start, int64 limit) {
-      static const int64 kPacketSize = (sizeof(Packet) / sizeof(T));
-      const int64 input_image_size =
-          args.in_rows * args.in_cols * args.in_depth;
-      const int64 output_image_size =
-          args.out_rows * args.out_cols * args.out_depth;
-      const int64 filter_spatial_size = args.filter_rows * args.filter_cols;
-      const int64 padded_filter_inner_dim_size =
-          ((args.out_depth + kPacketSize - 1) / kPacketSize) * kPacketSize;
-
-      // Allocate buffer for local input regions.
-      Tensor input_buffer;
-      OP_REQUIRES_OK(
-          ctx, ctx->allocate_temp(DataTypeToEnum<T>::value,
-                                  TensorShape({filter_spatial_size,
-                                               padded_filter_inner_dim_size}),
-                                  &input_buffer));
-      T* input_buffer_data = input_buffer.template flat<T>().data();
-
-      for (int64 i = start; i < limit; ++i) {
-        const int64 b = i / args.out_rows;
-        const int64 in_base = b * input_image_size;
-        const int64 out_base = b * output_image_size;
-
-        const int64 out_r = i % args.out_rows;
-
-        for (int64 out_c = 0; out_c < args.out_cols; ++out_c) {
-          // Populate 'input_buffer_data' with data from local input region.
-          functor::HexRotDepthwiseInputCopyOp<T>()(args, padded_filter_inner_dim_size,
-                                             out_r, out_c, input + in_base,
-                                             input_buffer_data);
-
-          // Process buffered input across all filters and store to output.
-          HexRotDepthwiseConv2DKernel<T>::Run(
-              args, padded_filter_inner_dim_size, out_r, out_c, filter_data,
-              input_buffer_data, output + out_base, data_format);
-        }
-      }
-    };
-
-    const int64 total_shards = args.batch * args.out_rows;
-
-    // Empirically tested to give reasonable performance boosts at batch size 1
-    // without reducing throughput at batch size 32.
-    const float kCostMultiplier = 2.5f;
-
-    // TODO(andydavis): Estimate shard cost (in cycles) based on the number of
-    // flops/loads/stores required to compute one shard.
-    const int64 shard_cost = kCostMultiplier * args.out_cols * args.out_depth;
-
-    auto worker_threads = *(ctx->device()->tensorflow_cpu_worker_threads());
-    Shard(worker_threads.num_threads, worker_threads.workers, total_shards,
-          shard_cost, shard);
-  }
-};
-
 // Extern template instantiated in conv_ops.cc.
 extern template class LaunchConv2DOp<CPUDevice, float>;
 
@@ -252,7 +65,6 @@ extern template class LaunchConv2DOp<CPUDevice, float>;
 extern template struct LaunchHexRotDepthwiseConvOp<GPUDevice, Eigen::half>;
 extern template struct LaunchHexRotDepthwiseConvOp<GPUDevice, float>;
 extern template struct LaunchHexRotDepthwiseConvOp<GPUDevice, double>;
-
 // Extern template instantiated in conv_ops.cc.
 extern template class LaunchConv2DOp<GPUDevice, float>;
 
@@ -296,10 +108,12 @@ class HexRotDepthwiseConv2dNativeOp : public BinaryOp<T> {
     // Input tensor is of the following dimensions:
     // [ batch, in_rows, in_cols, in_depth ]
     const Tensor& input = context->input(0);
-
     // Input filter is of the following dimensions:
     // [ filter_rows, filter_cols, in_depth, depth_multiplier]
     const Tensor& filter = context->input(1);
+    // Input rotation tensor, with the following dimensions.
+    // [ batch, in_rows, in_cols, in_depth ]
+    const Tensor& rotation = context->input(2);
 
     // For 2D convolution, there should be 4 dimensions.
     OP_REQUIRES(context, input.dims() == 4,
@@ -388,9 +202,11 @@ class HexRotDepthwiseConv2dNativeOp : public BinaryOp<T> {
 
     auto input_ptr = input.template flat<T>().data();
     auto filter_ptr = filter.template flat<T>().data();
+    auto rotation_ptr = rotation.template flat<T>().data();
     auto output_ptr = output->template flat<T>().data();
-    LaunchHexRotDepthwiseConvOp<Device, T>()(context, args, input_ptr, filter_ptr,
-                                       output_ptr, data_format_);
+
+    LaunchHexRotDepthwiseConvOp<Device, T>()(
+        context, args, input_ptr, filter_ptr, output_ptr, data_format_);
   }
 
  private:
